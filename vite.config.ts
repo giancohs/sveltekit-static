13d5ea9ae3746959dc55b5a152dfbc71c9900451
giancohs <giancohs@gmail.com>
2024-12-05 16:09:41 +0000

    deploying static boilerplate

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,14 @@
-import { defineConfig } from 'vitest/config';
 import { sveltekit } from '@sveltejs/kit/vite';
+import { defineConfig } from 'vite';
 
 export default defineConfig({
 	plugins: [sveltekit()],
-
-	test: {
-		include: ['src/**/*.{test,spec}.{js,ts}']
+	server: {
+		host: '0.0.0.0',
+		port: 9173,
+		strictPort: true,
+		watch: {
+			usePolling: true
+		}
 	}
 });
